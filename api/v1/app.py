fd533cd34a5b7c971538c904bf05ba3cdff30af2
Sir-Leon1 <eb3.61594.22@student.chuka.ac.ke>
2024-08-06 17:38:01 +0300

    Error handler for 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -18,6 +18,12 @@ def teardown_db(exception):
     storage.close()
 
 
+@app.errorhandler(404)
+def not_found(error):
+    """Error handler for 404 error"""
+    return {"error": "Not found"}, 404
+
+
 if __name__ == "__main__":
     app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
             port=int(getenv('HBNB_API_PORT', 5000)),
